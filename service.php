49ebefc1ff3c1d517e0e40b1d23343e0064ecff5
Kittikon Thamakittipan <76489651+kittikon53@users.noreply.github.com>
2023-10-26 23:50:14 +0700

    service

---
+++ service.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Our Service in HTML and CSS</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h1>Our Services</h1> I
+    <div class="row">
+        <div class="box">
+        <img src="Logo Followup.png" width="500px" height="250px">
+            <h3>Follow up</h3>
+            <p>คือ ระบบจัดการและวางแผนงานที่ถูกออกแบบเพื่อช่วยให้คุณสามารถจัดการและวางแผนงานต่าง ๆ อย่างมีประสิทธิภาพและง่ายต่อการใช้งาน</p>
+        </div>
+
+        <div class="box">
+            <img src="plan.jpg" width="500px" height="250px">
+            <h3>Planner</h3>
+            <p>คือ การสร้างแผนงานและกำหนดรายละเอียดของงานวันกำหนดส่งของงานและความสำคัญของแต่ละงาน</p>
+        </div>
+
+        <div class="box">
+            <img src="calendar.jpg" width="500px" height="250px">
+            <h3>Due date</h3>
+            <p>คือ วันครบกำหนดส่งงานโดยให้ผู้ใช้กำหนดเวลาสิ้นสุดของงานได้เอง</p>
+        </div>
+    </div>
+</body>
+</html>
